874cd3ae8d35853beb29ed4f6b09c73080769c28
DICE_shenyu <jisuanji888888@outlook.com>
2022-12-21 02:54:45 +0800

    Update background

--- server/background.py
+++ server/background.py
@@ -1,4 +1,10 @@
+import json
 from flask import request
+
+from constants import CONFIG_PATH
+from utils import read_json
+from core.database import userData
+from core.Account import Account
 
 
 def backgroundSetBackground():
@@ -6,12 +12,48 @@ def backgroundSetBackground():
     data = request.data
     request_data = request.get_json()
 
+    secret = request.headers.get("secret")
+    bgID = request_data["bgID"]
+    server_config = read_json(CONFIG_PATH)
+    
+    if not server_config["server"]["enableServer"]:
+        data = {
+            "statusCode": 400,
+            "error": "Bad Request",
+            "message": "Server is close"
+        }
+        return data
+    
+    result = userData.query_account_by_secret(secret)
+    
+    if len(result) != 1:
+        data ={
+            "result": 2,
+            "error": "此账户不存在"
+        }
+        return data
+    
+    accounts = Account(*result[0])
+
+    if accounts.get_ban() == 1:
+        data = {
+            "statusCode": 403,
+            "error": "Bad Request",
+            "message": "Your account has been banned"
+        }
+        return data
+    
+    player_data = json.loads(accounts.get_user())
+    player_data["background"]["selected"] = bgID
+
+    userData.set_user_data(accounts.get_uid(), player_data)
+
     data = {
         "playerDataDelta": {
             "deleted": {},
             "modified": {
                 "background": {
-                    "selected": request_data["bgID"]
+                    "selected": bgID
                 }
             }
         }
